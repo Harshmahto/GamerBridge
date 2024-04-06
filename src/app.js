257eb4909945fc9d9a763e42be4a0b5aec2b6368
Harshmahto <62500223+Harshmahto@users.noreply.github.com>
2024-04-06 19:32:47 +0530

    monogo connect

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+// Routes imports
+// import userRouter from "./routes/user.routes.js";
+// import pdfsRouter from "./routes/pdfs.routes.js"
+
+// Router declaration
+// app.use("/api/v1/user",userRouter)
+// app.use("/api/v1/pdfs",pdfsRouter)
+
+
+
+export { app }
